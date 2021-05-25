5f1f36b02deb98ccbc018a76c76f8590cafe4a0b
LuisYanardi25 <38727375+LuisYanardi25@users.noreply.github.com>
2021-05-25 13:10:11 -0300

    Add files via upload

---
+++ Ejercicio 5/js/data.js
@@ -0,0 +1,9 @@
+const Persona = [{
+    Nombre = "Pepe",
+    Apellido = "Don Pepe",
+    Biografia = "Nacido en XXXXXX, lugar XXXXXX"
+    Telefono = {
+        "15484878",
+        "1547854",
+    }
+}];
---
+++ Ejercicio 5/js/data2.js
@@ -0,0 +1,9 @@
+ cont Persona = [{
+     Nombre = "Pepa",
+     Apellido = "Don Pepa",
+     Biografia = "Nacido en XXXXXX, lugar XXXXXX"
+     Telefono = {
+         "15481238",
+         "151233333",
+     }
+ }];
